9e04ce6e5be0bbbf05c5730523d5ace36adb020e
ahnaf <ahnafsakibalam@yahoo.com>
2020-02-14 11:12:13 +1100

    new

--- example/views.py
+++ example/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import HttpResponse
-
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
 
 # Create your views here.
- index(request):
- return HttpResponse("hello prithibi")
+def index(request):
+    return HttpResponse("hello prithibi")
+
+
  
